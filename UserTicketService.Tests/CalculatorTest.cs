ca9e77c9e9e683f2db94cb0a8fd9d510a0210c3e
Oix135 <oix@mail.ru>
2024-02-16 10:21:20 +0300

    Добавьте файлы проекта.

---
+++ UserTicketService.Tests/CalculatorTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace UserTicketService.Tests
+{
+    public class CalculatorTest
+    {
+        [TestFixture]
+        public class CalculatorTests
+        {
+            [Test]
+            public void AdditionalMustReturnCorrectValue()
+            {
+                var calculatorTest = new Calculator();
+                Assert.That(calculatorTest.Additional(10, 5), Is.EqualTo(15));
+            }
+            [Test]
+            public void SubtractionMustReturnCorrectValue()
+            {
+                var calculatorTest = new Calculator();
+                Assert.That(calculatorTest.Subtraction(10, 5), Is.EqualTo(5));
+            }
+            [Test]
+            public void MiltiplicationMustReturnCorrectValue()
+            {
+                var calculatorTest = new Calculator();
+                Assert.That(calculatorTest.Miltiplication(10, 5), Is.EqualTo(50));
+            }
+            [Test]
+            public void DivisionMustReturnCorrectValue()
+            {
+                var calculatorTest = new Calculator();
+                Assert.That(calculatorTest.Division(10, 5), Is.EqualTo(2));
+            }
+        }
+    }
+}
